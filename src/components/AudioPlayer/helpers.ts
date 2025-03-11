aebee3629d5c487627c768ed74edbf366b8502e6
Nikolay Sayanskii <sarody@ya.ru>
2025-03-11 15:52:33 +0300

    tsconfig.node.json

--- src/components/AudioPlayer/helpers.ts
+++ src/components/AudioPlayer/helpers.ts
@@ -4,8 +4,3 @@ export const formatTime = (time: number): string => {
     const seconds = Math.floor(time % 60);
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 };
-
-export const calculateProgressPercentage = (): number => {
-    if (!duration) return 0;
-    return (currentTime / duration) * 100;
-};
